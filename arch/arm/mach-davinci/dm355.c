dbd2c5dc5f8e704519b52996672dfd91237db822
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2019-01-30 13:58:53 +0100

    ARM: davinci: aintc: use the new irqchip config structure in dm* SoCs
    Add the new-style config structures for dm* SoCs. They will be used
    once we make the aintc driver stop using davinci_soc_info.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- arch/arm/mach-davinci/dm355.c
+++ arch/arm/mach-davinci/dm355.c
@@ -15,6 +15,7 @@
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
 #include <linux/init.h>
+#include <linux/irqchip/irq-davinci-aintc.h>
 #include <linux/platform_data/edma.h>
 #include <linux/platform_data/gpio-davinci.h>
 #include <linux/platform_data/spi-davinci.h>
@@ -737,6 +738,16 @@ int __init dm355_init_video(struct vpfe_config *vpfe_cfg,
 
 	return 0;
 }
+
+static const struct davinci_aintc_config dm355_aintc_config = {
+	.reg = {
+		.start		= DAVINCI_ARM_INTC_BASE,
+		.end		= DAVINCI_ARM_INTC_BASE + SZ_4K,
+		.flags		= IORESOURCE_MEM,
+	},
+	.num_irqs		= 64,
+	.prios			= dm355_aintc_prios,
+};
 
 void __init dm355_init_irqs(void)
 {
