29168d8c6500614773c979cbebb44f58f0acbc73
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2019-01-30 16:30:01 +0100

    ARM: davinci: cp-intc: add a new config structure
    Add a config structure that will be used by cp-intc-based platforms.
    It contains the register range resource and the number of interrupts.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

---
+++ include/linux/irqchip/irq-davinci-cp-intc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2019 Texas Instruments
+ */
+
+#ifndef _LINUX_IRQ_DAVINCI_CP_INTC_
+#define _LINUX_IRQ_DAVINCI_CP_INTC_
+
+#include <linux/ioport.h>
+
+struct davinci_cp_intc_config {
+	struct resource reg;
+	unsigned int num_irqs;
+};
+
+#endif /* _LINUX_IRQ_DAVINCI_CP_INTC_ */
